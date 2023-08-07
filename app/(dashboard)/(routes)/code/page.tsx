b678dd5ad99db96dda5c1f07852a2600da1dfbc7
Mohahmad0 <98186675+Mohahmad0@users.noreply.github.com>
2023-08-07 08:51:20 -0500

    code  and conversation complete w/ api's

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -9,12 +9,13 @@ import { ChatCompletionRequestMessage } from "openai";
 
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { MessageSquare } from "lucide-react";
+import { Code, Divide } from "lucide-react";
 
 import { useRouter } from "next/navigation";
 
 import { formSchema } from "./constants";
 
+import ReactMarkdown from "react-markdown";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/heading";
@@ -27,7 +28,7 @@ import { BotAvatar } from "@/components/bot-avatar";
 
 
 // Conversations UI
-const ConversationPage = () => {
+const Codepage = () => {
     const router = useRouter();
     const[Messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
 
@@ -48,7 +49,7 @@ const ConversationPage = () => {
             };
             const newMessages = [...Messages, userMessage];
 
-            const response = await axios.post("/api/conversation", {
+            const response = await axios.post("/api/code", {
                 messages: newMessages,
             });
 
@@ -68,11 +69,11 @@ const ConversationPage = () => {
     return (
         <div>
             <Heading 
-                title="Conversation"
-                description="Our most advanced conversation model"
-                icon={MessageSquare}
-                iconColor="text-violet-500"
-                bgColor="bg-violet-500/10"
+                title="Code Generation"
+                description="Generate code using descriptive text."
+                icon={Code}
+                iconColor="text-green-700"
+                bgColor="bg-green-700/10"
             />
             <div className="px-4 lg:px-8">
                 <div>
@@ -90,7 +91,7 @@ const ConversationPage = () => {
                                             <Input 
                                             className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparant"
                                             disabled={isLoading}
-                                            placeholder="How do I calculate the radius of a circle?"
+                                            placeholder="Simple toggle button using react hooks."
                                             {...field}
                                             />
                                         </FormControl>
@@ -126,9 +127,21 @@ const ConversationPage = () => {
                             )}
                             >
                                 {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
-                                <p className="test-sm">
-                                    {message.content}
-                                </p>
+                                <ReactMarkdown
+                                    components={{
+                                        pre: ({ node, ...props }) => (
+                                            <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                                                <pre {...props} />
+                                            </div>
+                                        ),
+                                        code: ({ node, ...props }) => (
+                                            <code className="bg-black/10 rounded-lg p-1" {...props} />
+                                        )
+                                    }}
+                                    className="text-sm overflow-hdden leading-7"
+                                >
+                                    {message.content || ""}
+                                </ReactMarkdown>
                             </div>
                         ))}
                     </div>
@@ -138,4 +151,4 @@ const ConversationPage = () => {
     );
 }
  
-export default ConversationPage;
+export default Codepage;
